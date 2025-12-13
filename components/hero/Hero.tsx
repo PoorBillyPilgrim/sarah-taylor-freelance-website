ac98c7582afc9ccef0b1c8e4265e87ef1ac75e80
Tyler Jones <tyler.jones@lyrasis.org>
2025-12-13 12:30:18 -0800

    init cards in services section

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -12,51 +12,55 @@ import classes from "./Hero.module.css";
 
 export function Hero() {
   return (
-    <Container component="section" size="xl" bg="green.9" pos="relative">
-      <Flex
-        px={{ base: 50, lg: 100 }}
-        className={classes.inner}
-        direction={{ base: "column", md: "row-reverse" }}
-        justify={{ base: "space-between" }}
-      >
-        <Image
-          src="/climbing.jpg"
-          className={classes.image}
-          radius="100%"
-          style={{ zIndex: 1 }}
-        />
+    <Box component="section" bg="green.9" pos="relative">
+      <Container size="xl">
+        <Flex
+          px={{ base: 50, lg: 100 }}
+          className={classes.inner}
+          direction={{ base: "column", md: "row-reverse" }}
+          justify={{ base: "space-between" }}
+          align="center"
+        >
+          <Image
+            src="/climbing.jpg"
+            className={classes.image}
+            radius="100%"
+            pt={{ base: 30 }}
+            style={{ zIndex: 1 }}
+          />
 
-        <div className={classes.content}>
-          <Title className={classes.title} c="white">
-            Copywriter for <em>adventurous</em> brands
-          </Title>
-          <Text c="white" mt="md">
-            Get noticed and grow your business wth copy that converts, content
-            that builds credibility, and strategy that scales
-          </Text>
+          <div className={classes.content}>
+            <Title className={classes.title} c="white">
+              Copywriter for <em>adventurous</em> brands
+            </Title>
+            <Text c="white" mt="md">
+              Get noticed and grow your business wth copy that converts, content
+              that builds credibility, and strategy that scales
+            </Text>
 
-          <Group mt={30}>
-            <Button
-              color="orange.5"
-              radius="md"
-              size="md"
-              className={classes.control}
-            >
-              Let's Go
-            </Button>
-          </Group>
-        </div>
-      </Flex>
+            <Group mt={30}>
+              <Button
+                color="orange.5"
+                radius="md"
+                size="md"
+                className={classes.control}
+              >
+                Let's Go
+              </Button>
+            </Group>
+          </div>
+        </Flex>
+      </Container>
       <Box
         visibleFrom="md"
         w="100%"
         bg="white"
-        h="200"
+        h="100"
         px="0"
         pos="absolute"
         bottom="0"
         left="0"
       />
-    </Container>
+    </Box>
   );
 }
--- components/services/Services.tsx
+++ components/services/Services.tsx
@@ -1,5 +1,41 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { Container, Title } from "@mantine/core";
+import {
+  faClipboardCheck,
+  faDesktop,
+  faPencil,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Box,
+  Center,
+  Container,
+  Grid,
+  Group,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import Icon from "../icon/Icon";
+
+const cards = [
+  {
+    id: 1,
+    icon: faDesktop,
+    title: "Copywriting",
+    text: "Stand out and sell more with action-inspiring web copy, objection-busting email sequences, and expert-level marketing literature.",
+  },
+  {
+    id: 2,
+    icon: faPencil,
+    title: "Content Writing",
+    text: "Build brand awareness and authority with SEO-friendly blog posts, attention-grabbing newsletters, and proof-packed case studies.",
+  },
+  {
+    id: 3,
+    icon: faClipboardCheck,
+    title: "Content Strategy + Consulting",
+    text: "Ensure every piece of content is empowered by purpose and delivers the desired results.",
+  },
+];
 
 const Card = ({
   icon,
@@ -10,23 +46,41 @@ const Card = ({
   title: string;
   text: string;
 }) => {
-  return <div></div>;
+  return (
+    <Grid gutter={{ base: "50" }}>
+      <Grid.Col span={{ base: 12, sm: "auto" }}>
+        <Icon icon={icon} size="5x" />
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, sm: 9 }}>
+        <Stack>
+          <Title order={3} c="white">
+            {title}
+          </Title>
+          <Text c="white">{text}</Text>
+        </Stack>
+      </Grid.Col>
+    </Grid>
+  );
 };
 
 const Services = () => {
   return (
-    <Container
-      id="services"
-      component="section"
-      size="xl"
-      bg="green.9"
-      px={{ base: 50, lg: 100 }}
-      py={{ base: 50 }}
-    >
-      <Title order={2} c="white">
-        Services
-      </Title>
-    </Container>
+    <Box id="services" component="section" bg="green.9">
+      <Container size="xl" px={{ base: 50, lg: 100 }} py={{ base: 50 }}>
+        <Title order={2} c="white">
+          Services
+        </Title>
+
+        {cards.map(card => (
+          <Card
+            key={card.id}
+            icon={card.icon}
+            title={card.title}
+            text={card.text}
+          />
+        ))}
+      </Container>
+    </Box>
   );
 };
 
